5367356da4cacbf994514d16e940312a89ddf972
yohanes_prima <jooprima@gmail.com>
2019-10-07 10:42:21 +0700

    Tugas 1 PHP
    tugas ini dikumpulkan untuk niomic

---
+++ tugas1.php
@@ -0,0 +1,25 @@
+<?php
+  $a = "Rp. 13.000";
+  $b = "Rp. 15.000";
+  $c = " Rp. 13.000";
+  $d = "Rp. 1000";
+  $e = "Rp. 1000";
+  $f = "Rp. 3000";
+  $g = "Rp. 5000";
+  $h = "Rp. 4000";
+  $i = "Rp.5000";
+
+  echo "<h1>Daftar Menu</h1>";
+  echo "<br />";
+  echo "<ul>
+      <li>Ayam Goreng &nbsp&nbsp&nbsp = $a</li>
+      <li>Ayam Bakar &nbsp&nbsp&nbsp&nbsp&nbsp = $b</li>
+      <li>Ayam Sayur &nbsp&nbsp&nbsp&nbsp&nbsp =$c</li>
+      <li>Tempe Goreng &nbsp = $d</li>
+      <li>Tahu goreng &nbsp&nbsp&nbsp&nbsp&nbsp = $e</li>
+      <li>Soto &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp = $f</li>
+      <li>Nasi Putih &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp = $g</li>
+      <li>Es Teh &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp = $h</li>
+      <li>Es Tebu &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp = $i</li>
+  </ul>";
+ ?>
---
+++ tugas1_2.php
@@ -0,0 +1,48 @@
+<?php
+  $cireng = "Rp.1000";
+  $Esduren = "Rp.15.000";
+  $esmilo = "Rp.2000";
+  $esteh = "Rp.5000";
+  $piscok = "Rp.1000";
+
+  echo "<h1>DAFTAR MENU</h1>";
+
+  echo "<Table border=1>
+          <tr>
+            <th>No</th>
+            <th>Nama Menu</th>
+            <th>Harga</th>
+          </tr>
+
+          <tr>
+            <td>1</td>
+            <td>Cireng</td>
+            <td>$cireng</td>
+          </tr>
+
+          <tr>
+            <td>2</td>
+            <td>Es Duren</td>
+            <td>$Esduren</td>
+          </tr>
+
+          <tr>
+            <td>3</td>
+            <td>Es Milo</td>
+            <td>$esmilo</td>
+          </tr>
+
+          <tr>
+            <td>4</td>
+            <td>Es Teh</td>
+            <td>$esteh</td>
+          </tr>
+
+          <tr>
+            <td>5</td>
+            <td>Piscok</td>
+            <td>$piscok</td>
+          </tr>
+        </table>";
+
+ ?>
